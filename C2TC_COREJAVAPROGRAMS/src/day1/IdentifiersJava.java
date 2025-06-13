98b37c85df0723e811b10df987ac3071c3bdbe36
ACT-C5-18 <ACT-C5-18@DESKTOP-I4SCAC5>
2025-06-13 10:34:49 +0530

    This is my identifier java program

---
+++ C2TC_COREJAVAPROGRAMS/src/day1/IdentifiersJava.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class IdentifiersJava {
+
+	public static void main(String[] args) {
+	int number = 20;
+	System.out.println("value of the number variable is :" +number);
+	
+	String studentname = "Jai Akash";
+	System.out.println("value is :"+studentname);
+
+	}
+
+}
