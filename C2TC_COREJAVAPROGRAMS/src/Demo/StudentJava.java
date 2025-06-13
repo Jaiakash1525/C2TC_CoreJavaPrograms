7b8064a3ff5ed8df1f82d3e15ff1c2a6f8136deb
ACT-C5-18 <ACT-C5-18@DESKTOP-I4SCAC5>
2025-06-13 12:20:51 +0530

    this is my getter and setter and java program

---
+++ C2TC_COREJAVAPROGRAMS/src/Demo/StudentJava.java
@@ -0,0 +1,39 @@
+package Demo;
+
+public class StudentJava {
+	private int id;
+	private String name;
+	private int age;
+	
+
+	
+	@Override
+	public String toString() {
+		return "StudentJava [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+public void setId(int id)
+{
+	this.id=id;
+}
+	
+public int getId() {
+	return this.id;
+}
+}
