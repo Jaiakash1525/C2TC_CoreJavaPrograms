176451f00354941561711f60b2b7ee6872d32c59
ACT-C5-18 <ACT-C5-18@DESKTOP-I4SCAC5>
2025-06-16 13:43:41 +0530

    This is customer entity java program

---
+++ C2TC_COREJAVAPROGRAMS/src/Demo/Customer.java
@@ -0,0 +1,43 @@
+package Demo;
+
+public class Customer {
+
+       private int id;
+       private String name;
+       private String city;
+       
+       public void setId(int id){
+    	   this.id = id;
+    	   
+    	   
+    	   
+       }
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getId() {
+		return id;
+	}
+       
+	}
+
+
---
+++ C2TC_COREJAVAPROGRAMS/src/Demo/CustomerDemo.java
@@ -0,0 +1,25 @@
+package Demo;
+
+public class CustomerDemo {
+public static void main(String args[]) {
+
+		
+		Customer ct = new Customer();
+		ct.setId (101);
+		ct.setName ("Jai");
+		ct.setCity ("Chennai");
+		
+		System.out.println(ct);
+		
+	
+	Customer ct1 = new Customer();
+		ct1.setId (101);
+		ct1.setName ("Jai");
+		ct1.setCity ("Chennai");
+		
+		System.out.println(ct1);
+		
+		
+	}
+
+}
