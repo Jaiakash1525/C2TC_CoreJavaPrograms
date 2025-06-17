57bbd20afafea9647ba5519aa1229ad9b16e2580
ACT-C5-18 <ACT-C5-18@DESKTOP-I4SCAC5>
2025-06-17 09:18:06 +0530

    This is my oops concept and encapsulation in java program

---
+++ C2TC_COREJAVAPROGRAMS/src/day3/Encapsulation.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//object creation 
+				Oopsconcept obj = new Oopsconcept();
+				obj.setSerialNum(101);
+				obj.setName("Pooja");
+				obj.setAge(20);
+				System.out.println(obj);
+	}
+
+}
