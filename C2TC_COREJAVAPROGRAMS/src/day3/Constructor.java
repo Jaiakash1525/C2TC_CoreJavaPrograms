57753b735f559d6ac67e86c894800d74624390a7
ACT-C5-18 <ACT-C5-18@DESKTOP-I4SCAC5>
2025-06-18 11:47:18 +0530

    This is my constructor java program

--- C2TC_COREJAVAPROGRAMS/src/day3/Constructor.java
+++ C2TC_COREJAVAPROGRAMS/src/day3/Constructor.java
@@ -1,45 +1,50 @@
-package day3;
-import java.util.Scanner;
-public class Constructor {
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		String name, city;
-		int id;
-		System.out.print("Enter Customer Id : ");
-		id=sc.nextInt();
-		
-		sc.nextLine();
-		System.out.print("Enter Customer Name : ");
-		name=sc.nextLine();
-		
-		System.out.print("Enter Customer City : ");
-		city=sc.nextLine();
 
-		
-		System.out.println(" ");
-		
-		Customerdemo c1=new Customerdemo(); //default constructor invoked
-		c1.setCustomerName(name);
-		c1.setCustomerId(id);
-		c1.setCustomerCity(city);
-		System.out.println(c1);
-		
-		System.out.print("Enter Customer Id : ");
-		id=sc.nextInt();
-		
-		sc.nextLine();
-		System.out.print("Enter Customer Name : ");
-		name=sc.nextLine();
-		
-		System.out.print("Enter Customer City : ");
-		city=sc.nextLine();
-		
-		Customerdemo c2=new Customerdemo(name, id, city); //parameterized constructor invoked);
-		System.out.println(c2);
-		
-		sc.close();
-		
 
+
+	package day3;
+	import java.util.Scanner;
+	public class Constructor {
+		public static void main(String args[])
+		{
+			Scanner sc=new Scanner(System.in);
+			String name, city;
+			int id;
+			System.out.print("Enter Customer Id : ");
+			id=sc.nextInt();
+			
+			sc.nextLine();
+			System.out.print("Enter Customer Name : ");
+			name=sc.nextLine();
+			
+			System.out.print("Enter Customer City : ");
+			city=sc.nextLine();
+
+			
+			System.out.println(" ");
+			
+			Customerdemo c1=new Customerdemo(); //default constructor invoked
+			c1.setCustomerName(name);
+			c1.setCustomerId(id);
+			c1.setCustomerCity(city);
+			System.out.println(c1);
+			
+			System.out.print("Enter Customer Id : ");
+			id=sc.nextInt();
+			
+			sc.nextLine();
+			System.out.print("Enter Customer Name : ");
+			name=sc.nextLine();
+			
+			System.out.print("Enter Customer City : ");
+			city=sc.nextLine();
+			
+			Customerdemo c2=new Customerdemo(name, id, city); //parameterized constructor invoked);
+			System.out.println(c2);
+			
+			sc.close();
+			
+
+		}
 	}
-}
+
+
