5bfbc29bc8b4179c6b152c6feac14302fa1bada5
ACT-C5-18 <ACT-C5-18@DESKTOP-I4SCAC5>
2025-06-24 12:14:13 +0530

    This is my java program using single inheritance

---
+++ C2TC_COREJAVAPROGRAMS/src/com/tnsif/day5/SingleIInheritance/Slinheritance.java
@@ -0,0 +1,12 @@
+package com.tnsif.day5.SingleIInheritance;
+
+public class Slinheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student student = new Student("Mbapee", "07", "Real madrid", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+
+}
